56fd51095cb58a1b60653cf1723b8b267e5dd6c1
Grégoire Thibaud <gregoire.thibo@gmail.com>
2023-05-28 12:33:35 +0200

    add base project

---
+++ gatsby-config.ts
@@ -0,0 +1,34 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+    jsxRuntime: `automatic`,
+    siteMetadata: {
+        siteUrl: "https://www.klip.se",
+        url: "https://www.klip.se",
+        title: "Klipse",
+        titleTemplate: "Klipse – %s",
+        description: "Vos yeux méritent d'être entraînés",
+        image: "/images/logo.png",
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Klipse`,
+                short_name: `Klipse`,
+                start_url: `/`,
+                background_color: `#FEF7EF`,
+                theme_color: `#FEF7EF`,
+                display: `standalone`,
+                icon: `static/images/logo.png`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+    ],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Title from '../components/Title';
+
+export default function Home() {
+    return (
+        <main>
+            <Title>Hello</Title>
+        </main>
+    );
+}
